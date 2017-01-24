7df019a9777c6908832078e78369cb89804d3518
Alex Simonian <alex.simonian@gmail.com>
2017-01-24 15:49:41 -0600

    As createadmin (#40)
    * Created admin interface
    
    * Delete sqlite3

--- bangazon/bangazon_api/admin.py
+++ bangazon/bangazon_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bangazon_api.models import Customer, Product, PaymentType, ProductType, Order
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(ProductType)
+admin.site.register(PaymentType)
+admin.site.register(Order)
+admin.site.register(Product)
+
+
+
+
