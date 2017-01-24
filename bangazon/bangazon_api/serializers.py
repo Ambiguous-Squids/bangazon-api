889a967381863ab97259b7212f3804e70178a62e
Rico <richardthomaswhitfield@gmail.com>
2017-01-24 15:19:33 -0600

    created PaymentType, ProductType, and Order serializer

--- bangazon/bangazon_api/serializers.py
+++ bangazon/bangazon_api/serializers.py
@@ -1,4 +1,39 @@
 from rest_framework import serializers
-from bangazon_api.models import *
+from bangazon_api.models import PaymentType, Order, ProductType
 
 
+class PaymentTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+	"""
+	Creates PaymentType Serializer
+	@rtwhitfield84
+
+	"""
+
+    class Meta:
+        model = PaymentType
+        fields = ('customer', 'first_name', 'last_name', 'account', 'expiration_date', 'ccv')
+
+class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+	"""
+	Creates ProductType Serializer
+	@rtwhitfield84
+
+	"""
+
+    class Meta:
+        model = ProductType
+        fields = ('category')
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+
+	"""
+	Creates Order Serializer
+	@rtwhitfield84
+
+	"""
+
+    class Meta:
+        model = Order
+        fields = ('active', 'customer', 'payment_type')
