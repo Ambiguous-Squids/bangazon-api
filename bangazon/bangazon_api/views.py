1ce77fd9afb6fc2a17d8745fe8c7ce491e5013d0
Nick Chemsak <nchemsak@gmail.com>
2017-01-25 11:20:38 -0600

    added views and url for payment types

--- bangazon/bangazon_api/views.py
+++ bangazon/bangazon_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from bangazon_api.models import PaymentType
+from bangazon_api.serializers import PaymentTypeSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class PaymentTypeViewSet(viewsets.ModelViewSet):
+    queryset = PaymentType.objects.all()
+    serializer_class = PaymentTypeSerializer
+
+    """
+    Creates PaymentType View
+    @nchemsak
+
+    """
