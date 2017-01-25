4a32f575eb68a4ec5802fa1183c22e3d85c55ef1
Matthew McCord <mattm.eternal@gmail.com>
2017-01-25 11:24:40 -0600

    added url(r'^admin/', admin.site.urls) for admin linking!

--- bangazon/bangazon/urls.py
+++ bangazon/bangazon/urls.py
@@ -24,5 +24,6 @@ router.register(r'customers', views.CustomerViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^admin/', admin.site.urls), 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
