d17f3788ea7b6c7e44a008df48753bf7d010145d
Matthew McCord <mattm.eternal@gmail.com>
2017-01-31 11:44:55 -0600

    deleted potentially confusing comment

--- bangazon/bangazon/urls.py
+++ bangazon/bangazon/urls.py
@@ -9,7 +9,6 @@ router.register(r'customers', views.CustomerViewSet)
 router.register(r'orders', views.OrderViewSet)
 router.register(r'payment_types', views.PaymentTypeViewSet)
 router.register(r'product_types', views.ProductTypeViewSet)
-# router.register(r'customers', views.UserViewSet)
 
 
 urlpatterns = [
