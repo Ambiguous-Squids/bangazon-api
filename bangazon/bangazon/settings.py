adb0cb0e72571767981ccc58a32b640d7cc366b7
asimonia <alex.simonian@gmail.com>
2017-03-06 13:37:23 -0600

    Added environment variable for database

--- bangazon/bangazon/settings.py
+++ bangazon/bangazon/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+bang_db = os.environ.get('BANGAZON_DB')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bangazon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'BANGAZON_DB'),
     }
 }
 
